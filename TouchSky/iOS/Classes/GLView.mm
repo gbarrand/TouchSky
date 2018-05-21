eaf7863455659a03f3b095d3678a18f1cc0fd513
Guy Barrand <guy.barrand@gmail.com>
2018-05-21 07:42:01 +0200

    update

--- TouchSky/iOS/Classes/GLView.mm
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2010, Guy Barrand. All rights reserved.
-// See the file TouchSky.license for terms.
-
-//#include <TouchSky/main>
-#include "../../TouchSky/main"
-
-#define EXLIB_APP TouchSky
-
-#include <exlib/app/iOS/GLView_mm>
