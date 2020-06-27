9c5caba96e1ecafe547f681c240e2005fa45d088
Guy Barrand <guy.barrand@gmail.com>
2020-06-27 11:08:31 +0200

    update

--- TouchSky/Cocoa/GLView.mm
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2010, Guy Barrand. All rights reserved.
-// See the file TouchSky.license for terms.
-
-#include "../TouchSky/main"
-
-typedef TouchSky::main app_main_t;
-
-#import <exlib/app/Cocoa/GLView_mm>
